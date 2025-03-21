b4e007772d1ddc7bb1e92ad057e3c8cb42709c73
tulio-gabriel <tgct@aluno.ifnmg.edu.br>
2025-03-21 16:51:23 -0300

    s

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 let db;
 
 if (process.env.NODE_ENV == "production") {
-	db = { mongoURI: "mongodb+srv://tgct:GnHEDCJ0fCNrvvlo@blogapp-db.9lepv.mongodb.net/?retryWrites=true&w=majority&appName=blogapp-db" };
+	db = { mongoURI: "mongodb+srv://tgct:<password>@blogapp-db.9lepv.mongodb.net/?retryWrites=true&w=majority&appName=blogapp-db" };
 } else {
 	db = { mongoURI: "mongodb://localhost/blogapp" };
 }
